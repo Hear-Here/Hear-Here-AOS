ce3556556f61d2fe608e8a6e3bc0f946f49e0565
Hyomk <qet557@gmail.com>
2023-05-04 02:12:50 +0900

    feat : Map에 포스팅 마커 생성 구현

--- presentation/src/main/java/com/hearhere/presentation/features/main/MainViewModel.kt
+++ presentation/src/main/java/com/hearhere/presentation/features/main/MainViewModel.kt
@@ -1,6 +1,135 @@
 package com.hearhere.presentation.features.main
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hearhere.domain.model.Pin
 import com.hearhere.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import javax.inject.Inject
 
-class MainViewModel : BaseViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor() : BaseViewModel() {
+
+    private val _pinStateList = MutableLiveData<List<PinState>>(emptyList())
+    val pinStateList: LiveData<List<PinState>>
+        get() = _pinStateList
+
+    private val _events = MutableStateFlow<List<PinEvent>>(emptyList())
+    val events = _events.asStateFlow()
+
+    fun requestPins() {
+        _loading.postValue(true)
+        val tempList = arrayListOf<PinState>()
+        val repsonse: List<Pin> = listOf<Pin>(
+            Pin(
+                1, "https://artmug.kr/image/goods_img1/2/24528.jpg?ver=1668349176", 37.495081,
+                126.957395
+            ),
+            Pin(
+                2, "https://i1.sndcdn.com/artworks-000660272461-rmfvxq-t500x500.jpg", 37.493081,
+                126.957395
+            ),
+            Pin(
+                2,
+                "https://thumb.pann.com/tc_480/http://fimg5.pann.com/new/download.jsp?FileID=53302512",
+                37.492081,
+                126.958395
+            ),
+            Pin(
+                3,
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTF4pThwkGQrSpSRNQ7m3ZT25JtGkGE2dvqxPId0IO1jSnShCPfspcBhns6vPRupwrU14&usqp=CAU",
+                37.496081,
+                126.959395
+            ),
+            Pin(
+                4, "https://i1.sndcdn.com/artworks-CDyMPstbky5qw7oe-NfF8Pg-t240x240.jpg", 37.496081,
+                126.967395
+            ),
+            Pin(
+                5,
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk58wYEVvPBpoqZ-RBeb5XQARvxEwaMenAat6x-Qo86LxecYnsABrf5CxPEtS0ZmQvD_s&usqp=CAU",
+                37.497081,
+                126.957695
+            ),
+            Pin(
+                1, "", 37.497081,
+                126.957395
+            ),
+            Pin(
+                1, "", 37.497081,
+                126.957595
+            ),
+        )
+        repsonse.forEach {
+            tempList.add(PinState(it, null))
+        }
+        fetchPins(tempList)
+    }
+
+    private fun fetchPins(list: List<PinState>) {
+        if (list.isEmpty()) return
+        val newPinList = arrayListOf<PinState>()
+
+        list.forEach { item ->
+            if (item.pin.imageUrl.isNullOrEmpty()) newPinList.add(item)
+            else newPinList.add(item.copy(bitmap = loadUrlToBitmap(item.pin.imageUrl!!)))
+            Log.e("item", item.toString())
+        }
+
+        _pinStateList.postValue(newPinList)
+        _loading.postValue(false)
+
+        addEvent(PinEvent.onCompletedLoad)
+    }
+
+
+    private fun loadUrlToBitmap(url: String): Bitmap? {
+        var bitmap: Bitmap? = null
+        val uThread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    val conn = URL(url).openConnection() as HttpURLConnection
+                    conn.doInput = true
+                    conn.connect()
+                    val `is` = conn.inputStream
+                    bitmap = BitmapFactory.decodeStream(`is`)
+                } catch (e: MalformedURLException) {
+                    e.printStackTrace()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        uThread.start()
+
+        try {
+            uThread.join()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        return bitmap
+    }
+
+    private fun addEvent(event: PinEvent) {
+        _events.update { it + event }
+    }
+
+    fun consumeViewEvent(event: PinEvent) {
+        _events.update { it - event }
+    }
+
+    data class PinState(val pin: Pin, var bitmap: Bitmap?)
+    sealed class PinEvent {
+        object onCompletedLoad : PinEvent()
+    }
 }
---
+++ presentation/src/main/java/com/hearhere/presentation/util/getCircleBitmap.kt
@@ -0,0 +1,28 @@
+package com.hearhere.presentation.util
+
+import android.graphics.*
+import android.os.Build
+import androidx.annotation.RequiresApi
+import kotlin.math.min
+
+@RequiresApi(Build.VERSION_CODES.N)
+fun Bitmap.getCircledBitmap(): Bitmap {
+    val squareBitmapWidth = min(this.width, this.height)
+    val dstBitmap = Bitmap.createBitmap(
+        squareBitmapWidth, squareBitmapWidth, Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(dstBitmap)
+    val paint = Paint()
+    paint.isAntiAlias = true
+
+    val rect = Rect(0, 0, squareBitmapWidth, squareBitmapWidth)
+    val rectF = RectF(rect)
+
+    canvas.drawOval(rectF, paint)
+    paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+    val left = ((squareBitmapWidth - this.width) / 2).toFloat()
+    val top = ((squareBitmapWidth - this.height) / 2).toFloat()
+    canvas.drawBitmap(this, left, top, paint)
+    this.recycle()
+    return dstBitmap
+}
