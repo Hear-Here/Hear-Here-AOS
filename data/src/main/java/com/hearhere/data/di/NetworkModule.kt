15f4ac232caab45060ce01ecd3f0a852eb856747
Hyomk <qet557@gmail.com>
2023-05-09 00:34:04 +0900

    feat : annotation 추가 및 Timeout 연장

--- data/src/main/java/com/hearhere/data/di/NetworkModule.kt
+++ data/src/main/java/com/hearhere/data/di/NetworkModule.kt
@@ -12,10 +12,22 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+//import okhttp3.logging.HttpLoggingInterceptor
+//import retrofit2.Retrofit
+//import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
 import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 object NetworkModule {
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class api
+
+
+
 
     @Provides
     fun provideBaseUrl() = "http://BuildConfig.BASE_URL" //추후 추가
@@ -26,25 +38,34 @@ object NetworkModule {
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
         OkHttpClient.Builder()
+            .connectTimeout(3, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
             .addInterceptor(loggingInterceptor)
             .build()
     }else{
         OkHttpClient
             .Builder()
+            .connectTimeout(3, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
             .build()
     }
 
     @Singleton
     @Provides
+    @api
     fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL:String): Retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
         .baseUrl(BASE_URL)
         .client(okHttpClient)
         .build()
 
-    @Provides
+
     @Singleton
-    fun provideApiService(retrofit: Retrofit) = retrofit.create(HearHereApiService::class.java)
+    @Provides
+    @api
+    fun provideApiService(@api retrofit: Retrofit) = retrofit.create(HearHereApiService::class.java)
 
     @Provides
     @Singleton
