2a91b81ea6aea9a8859fd91d9a2b3d2f3e8c0792
Hyomk <qet557@gmail.com>
2023-05-09 18:42:50 +0900

    add : enum & resource 추가

---
+++ presentation/common/src/main/java/com/hearhere/presentation/common/component/emojiButton/GenreType.kt
@@ -0,0 +1,6 @@
+package com.hearhere.presentation.common.component.emojiButton
+
+
+enum class GenreType(val kor : String) {
+    BALLAD("발라드"), DANCE("댄스"), HIPHOP("랩/힙합"), INDIE("인디"), POP("팝"), BAND("밴드"), TROT("트로트")
+}
---
+++ presentation/common/src/main/java/com/hearhere/presentation/common/component/emojiButton/WithType.kt
@@ -0,0 +1,5 @@
+package com.hearhere.presentation.common.component.emojiButton
+
+enum class WithType(val kor: String) {
+    ALONE("혼자"), FRIEND("친구"), COUPLE("연인"), FAMILY("가족"), PET("반려동물"), SOMEBODY("누군가")
+}
