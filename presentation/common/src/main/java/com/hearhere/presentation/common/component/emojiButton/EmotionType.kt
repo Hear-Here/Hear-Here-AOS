26a50a24ed989ec809aad1e30b1cd81def3cee85
Hyomk <qet557@gmail.com>
2023-06-30 02:25:41 +0900

    add : EmojiType Enum 한글 추가

--- presentation/common/src/main/java/com/hearhere/presentation/common/component/emojiButton/EmotionType.kt
+++ presentation/common/src/main/java/com/hearhere/presentation/common/component/emojiButton/EmotionType.kt
@@ -2,8 +2,8 @@ package com.hearhere.presentation.common.component.emojiButton
 
 import com.hearhere.presentation.common.R
 
-enum class EmotionType {
-    SMILE, SOSO, SAD, FUNNY, HEART, ANGRY
+enum class EmotionType(val kor: String) {
+    SMILE("행복한 기분"), SOSO("보통의 기분"), SAD("기분 슬픔"), FUNNY("즐거운 기분"), HEART("사랑에 빠진 기분"), ANGRY("화가난 기분")
 }
 
 fun EmotionType.getResource(): Int {
